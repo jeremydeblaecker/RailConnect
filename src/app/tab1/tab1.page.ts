7f824ce0e300a069df01240fd075a0271521145c
borkevo <julien.mercadal@ynov.com>
2022-01-21 16:25:22 +0100

    Push API Call Tab1
    Appel pour obtenir les gares proches de l'arrêt choisi (actuellement gare SNCF de Salon)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.readAPI('https://api.sncf.com/v1/coverage/sncf/stop_points/stop_point:SNCF:87753764:Train/places_nearby?key=0dca33cf-7a3b-4c16-9baf-534bbdaf98b6')
+    .subscribe((data) => {
+    console.log(data);
+  });
+  }
 
+  readAPI(URL: string) {
+    return this.http.get(URL);
+  }
 }
