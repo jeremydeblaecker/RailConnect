6dca6c9f2279b796fade75fce2fbe34435e28168
borkevo <julien.mercadal@ynov.com>
2022-01-27 11:21:15 +0100

    Push nearby places for Aix-en-Provence

--- src/app/accueil/accueil.module.ts
+++ src/app/accueil/accueil.module.ts
@@ -6,7 +6,6 @@ import { AccueilPage } from './accueil.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { AccueilPageRoutingModule } from './accueil-routing.module';
-import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -19,27 +18,4 @@ import { HttpClient } from '@angular/common/http';
   declarations: [AccueilPage]
 })
 
-export class AccueilPageModule {
-
-  constructor(private http: HttpClient) {
-    this.readAPI('https://api.sncf.com/v1/coverage/sncf/stop_points/stop_point:SNCF:87753764:Train/places_nearby?key=0dca33cf-7a3b-4c16-9baf-534bbdaf98b6')
-    .subscribe((data) => {
-    console.log(data);
-    this.stationData.nom = data['places_nearby']['1']['name'];
-    this.stationData.distance = data['places_nearby']['1']['distance'];
-    console.log(this.stationData.nom);
-    console.log(this.stationData.distance);  
-    });
-  }
-
-  readAPI(URL: string) {
-    return this.http.get(URL);
-  }
-
-  stationData = {
-    nom: '',
-    distance: ''
-  };
-
-
-}
+export class AccueilPageModule {}
--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -21,37 +21,46 @@ export class AccueilPage implements OnInit{
   ngOnInit() {
     this.storage.set('actualNav','tabs/accueil');
 
-    this.readAPI('https://api.sncf.com/v1/coverage/sncf/stop_points/stop_point:SNCF:87753764:Train/places_nearby?key=0dca33cf-7a3b-4c16-9baf-534bbdaf98b6')
-      .subscribe((data) => {
-      console.log('data: ', data);
-
-      this.data = data;
-      console.log('nom gare: ',this.data.places_nearby[0].name);
-
-      this.storage.set('DataApi',data);
-      this.storage.get('DataApi').then((val) => {
-        console.log('DataApi', val);
-      });
+    this.readAPI('https://api.sncf.com/v1/coverage/sncf/coord/5.443867%3B43.524823/places_nearby?key=0dca33cf-7a3b-4c16-9baf-534bbdaf98b6')
+    .subscribe((data) => {
+    console.log(data);
+    this.stationData.nom = data['places_nearby']['2']['name'];
+    this.stationData.distance = data['places_nearby']['2']['distance'];
+    this.stationData.id = data['places_nearby']['2']['id'];
+    console.log(this.stationData.nom);
+    console.log(this.stationData.distance);
+    console.log(this.stationData.id);   
     });
-
-  }
-
-  readAPI(URL: string) {
-    return this.http.get(URL);
-  }
-
-  favoris(){
-    const ionStar = (<HTMLIonIconElement>document.getElementById('ionStar'));
-    if (ionStar.name === 'star-outline' ){
-      ionStar.name = 'star';
     }
-    else{
-      ionStar.name = 'star-outline';
+
+    readAPI(URL: string) {
+      return this.http.get(URL);
     }
+    
+    stationData = {
+      nom: '',
+      distance: '',
+      id: ''
+    };
+
+    favoris(){
+      const ionStar = (<HTMLIonIconElement>document.getElementById('ionStar'));
+      if (ionStar.name === 'star-outline' ){
+        ionStar.name = 'star';
+      }
+      else{
+        ionStar.name = 'star-outline';
+      }
+    }
+  
+    goToGare(){
+      this.navCtrl.navigateForward('tabs/detail-gare');
+    }
+
   }
 
-  goToGare(){
-    this.navCtrl.navigateForward('tabs/detail-gare');
-  }
+  
+  
 
-}
+
+
