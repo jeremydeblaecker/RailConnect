74949c5bc90f3b60d3fb28dfc1f7acf6e9e79f9b
Muller Jimmy <43753915+Guirdin@users.noreply.github.com>
2022-01-27 13:14:05 +0100

    Modif infos profil
    Probleme HTMLElement avec .value

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -12,36 +12,44 @@ export class AccueilPage implements OnInit{
 
   data: any;
 
+  stationData = {
+      nom: '',
+      distance: '',
+      id: ''
+    };
+
   constructor(
     private http: HttpClient,
     private storage: Storage,
     public navCtrl: NavController,
   ) { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
     this.storage.set('actualNav','tabs/accueil');
-
     this.readAPI('https://api.sncf.com/v1/coverage/sncf/coord/5.443867%3B43.524823/places_nearby?key=0dca33cf-7a3b-4c16-9baf-534bbdaf98b6')
     .subscribe((data) => {
-    console.log(data);
-    this.stationData.nom = data['places_nearby']['2']['name'];
-    this.stationData.distance = data['places_nearby']['2']['distance'];
-    this.stationData.id = data['places_nearby']['2']['id'];
-    console.log(this.stationData.nom);
-    console.log(this.stationData.distance);
-    console.log(this.stationData.id);   
+      console.log(data);
+      this.data = data;
+
+      this.stationData.nom = this.data.places_nearby[2].name;
+      this.stationData.distance = this.data.places_nearby[2].distance;
+      this.stationData.id = this.data.places_nearby[2].id;
+
+      console.log('this.stationData.nom: ', this.stationData.nom);
+      console.log('this.stationData.distance: ', this.stationData.distance);
+      console.log('this.stationData.id: ', this.stationData.id);
+
+      this.storage.set('DataApi',data);
+      this.storage.get('DataApi').then((val) => {
+        console.log('DataApi', val);
+      });
     });
-    }
+  }
 
     readAPI(URL: string) {
       return this.http.get(URL);
     }
-    
-    stationData = {
-      nom: '',
-      distance: '',
-      id: ''
-    };
 
     favoris(){
       const ionStar = (<HTMLIonIconElement>document.getElementById('ionStar'));
@@ -52,15 +60,15 @@ export class AccueilPage implements OnInit{
         ionStar.name = 'star-outline';
       }
     }
-  
+
     goToGare(){
       this.navCtrl.navigateForward('tabs/detail-gare');
     }
 
   }
 
-  
-  
+
+
 
 
 
--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -7,6 +7,48 @@ import { Component } from '@angular/core';
 })
 export class ProfilPage {
 
+  modif: any = false;
+  pseudo: any = 'mon pseudo';
+  mail: any = 'monMail@mail.com';
+  actualPassword: any = '';
+  newPassword: any;
+
   constructor() {}
+
+  modifierInfos()
+  {
+    const inputPseudo = (document.getElementById('inputPseudo') as HTMLInputElement);
+    const inputMail = (document.getElementById('inputMail') as HTMLInputElement);
+    const inputOldPassword = (document.getElementById('inputOldPassword') as HTMLInputElement);
+    const inputNewPassword = (document.getElementById('inputNewPassword') as HTMLInputElement);
+
+    console.log('inputPseudo.value: ', inputPseudo.value);
+    console.log('inputMail.value: ', inputMail.value);
+    console.log('inputOldPassword.value: ', inputOldPassword.value);
+    console.log('inputNewPassword.value: ', inputNewPassword.value);
+
+    this.pseudo = inputPseudo.value;
+    this.newPassword = inputNewPassword.value;
+
+    if(this.actualPassword === inputOldPassword.value || this.pseudo !== inputPseudo.value || this.mail !== inputMail.value){
+      this.modif = false;
+    }
+  }
+
+  visibilityPassword(id){
+    const eyeChange = (document.getElementById(id) as HTMLIonIconElement);
+    let tabId: any[] = [];
+    tabId = eyeChange.id.split('_');
+    const input = (document.getElementById('input_' + tabId[1]) as HTMLInputElement);
+
+    if(eyeChange.name === 'eye-outline'){
+      eyeChange.name = 'eye-off-outline';
+      input.type = 'text';
+    }
+    else{
+      eyeChange.name = 'eye-outline';
+      input.type = 'password';
+    }
+  }
 
 }
