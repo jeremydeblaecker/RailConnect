5e5b1ef517cbe1497fbdf72d056bbfb0197d5e0c
jeremydeblaecker <jeremy.deblaecker@gmail.com>
2022-01-28 13:53:14 +0100

    Card detail

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Deeplinks } from '@ionic-native/deeplinks';
+import { AccueilPage } from './app/accueil/accueil.page';
+import { CreationProfilPage } from './app/creation-profil/creation-profil.page';
+import { DetailGarePage } from './app/detail-gare/detail-gare.page';
+
 
 if (environment.production) {
   enableProdMode();
